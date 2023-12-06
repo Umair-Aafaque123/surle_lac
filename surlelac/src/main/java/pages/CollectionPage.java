1f77cc00a66bb472c4c445ef596ae3441a6846bd
Umair-Aafaque123 <mohammadumairaafaque@gmail.com>
2023-12-06 15:51:49 +0530

    new changes

--- surlelac/src/main/java/pages/CollectionPage.java
+++ surlelac/src/main/java/pages/CollectionPage.java
@@ -31,7 +31,7 @@ public class CollectionPage
 	private WebElement checkOut;
 	 
 	
-	// umair aafaque shamim akhtar
+	// umair aafaque shamim akhtar waris pura
 	
 	public CollectionPage(WebDriver driver)   
 	{
